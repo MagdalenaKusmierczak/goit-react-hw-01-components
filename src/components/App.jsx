fdaacee83604ba8ef3204af3fe68abbbf0dd88ed
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-01-26 13:14:24 +0100

    Profile jsx structure, app structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+
+import Profile from './Profile/Profile.jsx';
+import Statistics from './Statistics/Statistics.jsx';
+import FriendList from './FriendList/FriendList.jsx';
+import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +17,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
