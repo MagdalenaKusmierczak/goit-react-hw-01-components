5f2a962cdca3d96bbaa48070ede179ac0e1c3366
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-01-27 14:52:14 +0100

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { getRandomHexColor } from './GetRandomColor.jsx';
+import { getRandomHexColor } from './getRandomColor.jsx';
 import {
   StatisticsSection,
   StatisticsTitle,
