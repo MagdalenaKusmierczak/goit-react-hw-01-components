f2fc2654193ef828bec6886c79d7b88e24c07c96
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2024-01-27 15:40:51 +0100

    fix

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -6,7 +6,7 @@ export default FriendListItem;
 function FriendListItem({ avatar, name, isOnline }) {
   return (
     <ListItem>
-      <Status status={isOnline}></Status>
+      <Status statusType={isOnline}></Status>
       <Avatar src={avatar} alt="User avatar" width="48" />
       <Name>{name}</Name>
     </ListItem>
